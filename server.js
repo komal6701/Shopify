325dd0e599f9660bd50f3b2c8380b351e415b52d
Komal <90527371+komal6701@users.noreply.github.com>
2023-09-30 12:37:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ import cors from 'cors'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import path from 'path'
+import {fileURLToPath} from 'url'
 //configure env
 dotenv.config();
 //database config
 connectDB();
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 
 const app = express();
 
